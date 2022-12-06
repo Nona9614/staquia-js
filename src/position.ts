4d0438b25254a3dce747fa92f26cebe9bca3d8ac
nona9614 <nona9614@hotmail.com>
2022-12-05 19:52:09 -0600

    Renaming to Position for simplification

--- src/position.ts
+++ src/position.ts
@@ -1,17 +1,15 @@
 import is from "guardex";
-import { system, after, before, diverge, between } from "./system";
 
-/**
- * Sizes from the position strings 
- */
-export const sizes = {
-    n: 4,
-    z: 12,
-    update({ n, z }: { n?: number, z?: number }) {
-        if (is.positive(n)) this.n = n;
-        if (is.positive(z)) this.z = z;
-    }
-}
+import type { NumberSystem, PositionValue } from "./types";
+
+// Utils
+import utils from "./system/utils";
+
+// Algorithms
+import between from "./system/algorithms/between";
+import diverge from "./system/algorithms/diverge";
+import after from "./system/algorithms/after";
+import before from "./system/algorithms/before";
 
 /**
  * This objects represents a postion like `zzzzsssssssss` where:
@@ -19,17 +17,22 @@ export const sizes = {
  *  - Z is a special string generated when an insertion between
  *    two positions with no room
 */
-export default class StaquiaPosition {
+export default class Position {
 
     /** Left number for after and before operations */
     public n: string = "";
     /** Right number for diverge and between operations */
     public z: string = "";
+    /** Reference number sytem */
+    #system: NumberSystem;
 
     /**
-     *  @param value Used to set a default value for the position
+     * @param system The number system to use for the position functions
+     * @param value Used to set a default value for the position
     */
-    constructor(value?: string | { n: string, z?: string }) {
+    constructor(system: NumberSystem, value?: PositionValue) {
+        this.#system = system;
+
         if (is.string(value)) {
             // Parses the default value and assing it
             this.parse(value);
@@ -40,7 +43,7 @@ export default class StaquiaPosition {
             this.parse(string);
         } else {
             // Starts the n with only zeroes
-            for (let i = 0; i < sizes.n; i++) this.n += system.zero;
+            for (let i = 0; i < system.ln; i++) this.n += system.zero;
             this.z = system.zero;
         };
     }
@@ -49,25 +52,25 @@ export default class StaquiaPosition {
      * Gets the relative next position from another position
     */
     public next() {
-        const n = after(this.n);
-        return new StaquiaPosition(n);
+        const n = after(this.#system, this.n);
+        return new Position(this.#system, n);
     }
 
     /**
      * Gets the relative next position from another position
     */
     public previous() {
-        const n = before(this.n);
-        return new StaquiaPosition(n);
+        const n = before(this.#system, this.n);
+        return new Position(this.#system, n);
     }
 
     /**
      * Returns the value that lies between the current postion and another
      * @param relative The position that should go before or after thi current position
      */
-    public core(relative: StaquiaPosition) {
-        let previous: StaquiaPosition = this;
-        let next: StaquiaPosition = relative;
+    public core(relative: Position) {
+        let previous: Position = this;
+        let next: Position = relative;
 
         // Set the proper weight
         if (previous > next) {
@@ -80,10 +83,10 @@ export default class StaquiaPosition {
         // if not use between
         const final: string = 
             previous.n === next.n || previous.next().n === next.n ?
-            diverge(previous.toString(), next.toString()) :
-            between(previous.n, next.n);
+            diverge(this.#system, previous.toString(), next.toString()) :
+            between(this.#system, previous.n, next.n);
         
-        return new StaquiaPosition(final);
+        return new Position(this.#system, final);
     }
 
     /**
@@ -91,21 +94,20 @@ export default class StaquiaPosition {
      * @param string Is the value to be parsed 
     */
     public parse(string: string) {
-        this.n = string.substring(0, sizes.n);
-        if (string.length > sizes.n) {
-            this.z = string.substring(sizes.n);
+        this.n = string.substring(0, this.#system.ln);
+        if (string.length > this.#system.ln) {
+            this.z = string.substring(this.#system.ln);
         } else {
-            this.n = system.untrim(this.n, sizes.n);
-            this.z = system.zero;
+            this.n = utils.untrim(this.#system, this.n, this.#system.ln);
+            this.z = this.#system.zero;
         }
-
     }
 
     /**
      * Updates the position based on the input values
     */
     public update({ n, z }: { n?: string, z?: string }) {
-        if (is.string(n)) this.n = system.untrim(n, sizes.n);
+        if (is.string(n)) this.n = utils.untrim(this.#system, n, this.#system.ln);
         if (is.string(z)) this.z = z;
     }
 
@@ -120,7 +122,7 @@ export default class StaquiaPosition {
      * Concatenates the `n` value with the `z`
     */
     public toString() {
-        return system.isZero(this.z) ? this.n : this.n + this.z;
+        return utils.isZero(this.#system, this.z) ? this.n : this.n + this.z;
     }
 
     /**
