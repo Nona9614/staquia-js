313c566bad2537b76345bcbfff9197c83faa84a7
nona9614 <nona9614@hotmail.com>
2022-12-08 23:44:45 -0600

    Making staquia a global object

--- src/staquia.ts
+++ src/staquia.ts
@@ -1,68 +1,104 @@
 import { merge } from "lodash";
+import { defaultSettings } from "./default-settings";
+import { handlers } from "./handlers";
 
-import Position from './position';
+import Position from "./position";
+import schemas from "./schemas";
 import createNumberSystem from "./system";
-import type { RequiredRecursive, StaquiaSettings, PositionValue, ThreseholdHandler, OverflowHandler, NumberSystem } from './types';
+import type { StaquiaSettings, PositionValue, NumberSystem } from "./types";
 
-const defaultSettings: RequiredRecursive<StaquiaSettings> = {
-  zero: "!",
-  first: "\u00B0",
-  last: "\u04B1",
-  ϰ: 0.15,
-  segments: {
-    /** `N` segment length */
-    n: 5,
-    /** `Z` segment length */
-    z: 15,
-  },
-  onThreshold(trigger) {},
-  onOverflow(trigger) {},
-}
-
-/**
- * A class that helps to have a dynamic position algorith using number systems
- * @example All that is menioned here are the default values
- * 
- * // An odd base (like 27th [a-z]) shoud be used with a zero character
- * // smaller than any value of the set (like [!])
- * const zero = "!"
- * const first = "a"
- * const last = "z"
- * 
- * // Sizes for the position algorithm, like `n = 4` and `z = 12`
- * // to generate positions like nnnn-zzzz-zzzz-zzzz
- * const size = { n: 4, z = 12 }
- */
-export class Staquia {
-
+class Staquia {
   /** Sytem used to forge positions */
   #system: NumberSystem;
 
-  /** Callback to trigger when entering the threshold */
-  #onThreshold: ThreseholdHandler
-
-  /** Callback to trigger during an overflow */
-  #onOverflow: OverflowHandler
-
   constructor(settings?: StaquiaSettings) {
-    const _settings = settings ? merge(defaultSettings, settings) : defaultSettings;
-    this.#system = createNumberSystem(_settings);
-    this.#onThreshold = _settings.onThreshold;
-    this.#onOverflow = _settings.onOverflow;
+    const _settings = settings
+      ? merge(defaultSettings, settings)
+      : defaultSettings;
+    const system = createNumberSystem(_settings);
+    schemas.ln = system.ln;
+    schemas.lz = system.lz;
+    handlers.onThreshold = _settings.onThreshold;
+    handlers.onOverflow = _settings.onOverflow;
+    this.#system = system;
+  }
+
+  /** Forges a recommended start position */
+  start() {
+    let n = "";
+    for (let i = 0; i < this.#system.ln; i++) {
+      n += this.#system.middle;
+    }
+    return this.position(n);
   }
 
   /** Forges a new position */
-  forge(value?: PositionValue) {
+  position(value?: PositionValue) {
     const position = new Position(this.#system, value);
-    const length = [...position.toString()].length;
-    if (this.#system.lmax >= length) {
-      this.#onOverflow(position, new Error(`The generated position '${position}' caused an overflow`));
-    }
-    else if (this.#system.lϰ >= length) this.#onThreshold(position);
     return position;
   }
-
 }
 
+/** Global staquia instance */
+let _instance = new Staquia();
+
+/**
+ * Staquia helps to have a dynamic lexicographical position algorithm.
+ * You can read more information in the documentation at:
+ * @link https://github.com/Nona9614/staquia-js
+ */
+const staquia = {
+  /**
+   * Updates the staquia settings
+   * @example
+   * // An odd base (like 27th [a-z]) should be used with a zero character
+   * // smaller than any value of the set (like [!])
+   * const zero = "!"
+   * const first = "a"
+   * const last = "z"
+   *
+   * // Segments for the position algorithm, that defines the length from the system
+   * const segments = { n: 4, z = 12 }
+   *
+   * // ϰ (kappa) Helps to set a threshold to fire events to handle possible overflows
+   * const ϰ = 0.15;
+   */
+  setup(settings?: StaquiaSettings) {
+    _instance = new Staquia(settings);
+  },
+  /**
+   * Creates a recommended start position to depart,
+   * to have the most possible position forges
+   * before an overflow
+   * @example
+   * // For instance if the middle character in the system is "n"
+   * // and the `n` segment length is `4`, the recommended position
+   * // would be "nnnn"
+   * const nnnn = staquia.start();
+   */
+  start() {
+    return _instance.start();
+  },
+  /**
+   * Creates a new position using the current staquia settings
+   * to generate positions like
+   * @example
+   * // You can create the position just like this
+   * const position = staquia.position("nnnnzzzz");
+   *
+   * // If you need it, you can get the object representation
+   * position.toObject() == { n: "nnnn", z: "zzzz" }
+   * // Or the string representation from your position
+   * position.toString() == "nnnnzzzz"
+   *
+   * // You can compare the instances with the less than `<`,
+   * // greater than `>` or equal `=` operators
+   * staquia.position("a") < staquia.position("b") == true
+   */
+  position(value?: PositionValue) {
+    return _instance.position(value);
+  },
+};
+
 // Force to export the settings here for rollup to expose them
-export default Staquia;
+export default staquia;
