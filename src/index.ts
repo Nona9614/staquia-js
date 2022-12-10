22a5f3113b9cfab2397bc8d7bc585efcfd6c00a1
nona9614 <nona9614@hotmail.com>
2022-12-10 00:41:43 -0600

    Changin references to a index file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { staquia } from "./staquia";
+
+// Export types
+export * from "./types";
+
+// Named exports
+export { staquia as staquia };
+
+// Exports staquia as default
+export default staquia;
