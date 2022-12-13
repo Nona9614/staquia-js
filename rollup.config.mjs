860a615dc9792def6cd8e61d7d69733eb57a43c9
nona9614 <nona9614@hotmail.com>
2022-12-13 16:04:15 -0600

    Exporting as CommonJS and ESM

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,28 @@
 // Contents of the file /rollup.config.js
 import ts from 'rollup-plugin-ts';
 import copy from 'rollup-plugin-copy';
+import pkg from './package.json' assert { type: "json" };
+
+/** Bases the paths to the `lib` folder */
+function lib(...paths) {
+  return paths.reduce((p, k) => p + `/${k}`, "lib")
+}
 
 /** @type {import('rollup').RollupOptions[]} */
 const config = [
   {
     input: 'src/index.ts',
-    output: {
-      name: 'staquia.js',
-      dir: 'lib',
-      format: 'cjs',
-      exports: 'named'
-    },
+    output: [
+      {
+        file: lib(pkg.module),
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        file: lib(pkg.main),
+        format: 'cjs',
+        sourcemap: true
+      }],
     external: ["guardex", "lodash"],
     plugins: [ts(), copy({
       targets: [
